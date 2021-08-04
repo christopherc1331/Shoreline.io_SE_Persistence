e93f6a5f0e60513dd4c6876718ac834ab71e39c2
Christopher Cruz <christopherc1331@gmail.com>
2021-08-04 00:06:51 -0700

    Finished 3rd solution

--- Middleware/src/main.go
+++ Middleware/src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"entities"
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -33,7 +34,9 @@ func indexOf(element string, data []string) int {
 	return -1    //not found.
 }
 
-func getCoursesByStudent(studentName string) ([]entities.Course, error) {
+// helper method for identifying which db a student can/should be located in
+func identifyDb(studentName string) string {
+
 	// 0-12th idx == A-M
 
 	var dbName string
@@ -46,6 +49,15 @@ func getCoursesByStudent(studentName string) ([]entities.Course, error) {
 	} else {
 		dbName = "[database_2]"
 	}
+
+	return dbName
+}
+
+func getCoursesByStudent(studentName string) ([]entities.Course, error) {
+	// 0-12th idx == A-M
+
+	dbName := identifyDb(studentName)
+
 	query := `
 		SELECT 
 			[CourseID],
@@ -134,4 +146,81 @@ func executeGetStudentsInCourseSql(dbName string, query string) []entities.Stude
 	students = append(students, student1, student2)
 
 	return students
+}
+
+func enrollStudent(name string, number string, courses []entities.Course) error {
+
+	dbName := identifyDb(name)
+
+	var courseIDs string
+
+	for idx := range courses {
+		if idx > 0 {
+			courseIDs += "," + strconv.FormatInt(courses[idx].CourseID, 10)
+
+		} else {
+			courseIDs += strconv.FormatInt(courses[idx].CourseID, 10)
+		}
+	}
+
+	query := `
+		SET NOCOUNT ON
+		SET XACT_ABORT ON
+
+		BEGIN TRY
+			BEGIN TRAN
+
+			INSERT INTO ` + dbName + `.[dbo].[Students] (
+				[StudentName],
+				[StudentPhoneNumber]
+			)
+			VALUES (
+				` + name + `,
+				` + number + `
+			)
+
+			DECLARE @StudentID INT = SCOPE_IDENTITY()
+			
+			INSERT INTO ` + dbName + `.[dbo].[Enrollment] (
+				[Enrollment_StudentID],
+				[Enrollment_CourseID],
+				[DateEnrolled],
+				[FinalGrade]
+			)
+			SELECT
+				@StudentID,
+				[value] AS [CourseID],
+				GETDATE() AS [DateEnrolled]',
+				NULL AS [FinalGrade]
+			FROM STRING_SPLIT(` + courseIDs + `, ',')
+
+			COMMIT TRAN
+		END TRY
+		
+		BEGIN CATCH
+		
+			IF XACT_STATE() = -1
+			BEGIN
+				ROLLBACK TRAN
+			END
+			IF XACT_STATE() = 1
+			BEGIN
+				COMMIT TRAN
+			END
+
+		END CATCH
+	`
+
+	err := executeEnrollStudentSql(dbName, query)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+// bogus executor to resolve errors in above function
+func executeEnrollStudentSql(dbName string, query string) error {
+	return nil
 }
